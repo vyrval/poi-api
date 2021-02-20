ca4fa9a967a8e297a5cf739adce5a24ac314a7ba
vyrval <alex77940@live.fr>
2021-02-20 10:48:57 +0100

    add mocha to env and fix lint

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const express = require('express');
 const app = express();
 
 let port = process.env.PORT;
-if (port == null || port == "") {
+if (!port || port === '') {
   port = 8080;
 }
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
