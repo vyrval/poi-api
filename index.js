612ea2afe4a88db3f46b91a76cc615874daa8ffe
vyrval <alex77940@live.fr>
2021-02-20 10:38:23 +0100

    prepare for heroku

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require('express');
 
 const app = express();
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8080;
+}
 
-app.listen(8080, () => {
-  console.log('Running');
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
 });
